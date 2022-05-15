48fc7cb8f2e239950998dc9de472435f21705718
yazgulesen <iamyazgul@gmail.com>
2022-05-14 23:26:23 -0400

    print numbers

---
+++ src/main/java/homework1/Hw1.java
@@ -0,0 +1,13 @@
+package homework1;
+
+public class Hw1 {
+    public static void main(String[] args) {
+        //HW1:
+        //1.create a new IJ project
+        //2.add code in it that prints the numbers from 1 to 100
+        //3.send this code to github
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
